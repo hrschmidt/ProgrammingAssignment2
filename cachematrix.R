8267b8a2630406116496146be545f130fe741a1d
hrschmidt <hugh_schmidt@hotmail.com>
2015-06-21 07:43:42 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are designed to leverage the multiple environments
+## that can be used in R by storing a value in a variable in one environment and 
+## then calling that variable in another using the cached value.
 
-## Write a short comment describing this function
-
+## Creates a vector that resolves to the mean of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+##Creates an empty matrix.
+m <- NULL
+##Sets the value of the vector.
+set <- function(y)
+{
+x <<- y
+m <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) m <<- inverse
+getinverse <- function() m
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## Calculates the inverse of the special “matrix” created in makeCacheMatrix
+## If inverse has been calculated, gets the inverse from the cache. If not in cache,
+## calculates the inverse of the data.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+m <- x$getinverse()
+if(!is.null(m)) {
+message("getting cached data")
+return(m)
+}
+data <- x$get()
+m <- solve(data, ...)
+x$setinverse(m)
+m
 }
